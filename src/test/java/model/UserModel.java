c3622fd55c119a8486efc37da12c7e84378eff82
Golam Mourshid <golam.mourshid@tigerit.com>
2024-01-31 09:27:43 +0600

    Added lombok dependency for getter and setter method

--- src/test/java/model/UserModel.java
+++ src/test/java/model/UserModel.java
@@ -1,5 +1,10 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserModel {
     private String email;
     private String password;
@@ -7,23 +12,6 @@ public class UserModel {
     public UserModel() {
 
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public UserModel(String email, String password) {
         this.email = email;
         this.password = password;
